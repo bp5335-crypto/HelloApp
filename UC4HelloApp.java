05ddc56dd69fc34e806aede7212d43b23c33e3a3
bp5335-beep <bp5335@srmist.edu.in>
2026-03-15 15:35:34 +0530

    Coded UC4: Display Hello with Multiple Command-Line Arguments

---
+++ UC4HelloApp.java
@@ -0,0 +1,13 @@
+public class UC4HelloApp {
+
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
+    }
+}
